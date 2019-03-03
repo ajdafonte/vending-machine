54aa5bd1d2343aa1e8288b06425721eb426e788d
Alejandro Caires <4990971+ajdafonte@users.noreply.github.com>
2019-03-03 10:27:24 +0100

    Update and clean gradle file

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,19 +1,11 @@
 plugins {
     java
-//    application
 }
 
 java {
     sourceCompatibility = JavaVersion.VERSION_1_8
     targetCompatibility = JavaVersion.VERSION_1_8
 }
-
-//application {
-//    group = "com.dexma.hometest"
-//    version = "1.0-SNAPSHOT"
-//    mainClassName = "com.dexma.hometest.Startup"
-//}
-
 
 repositories {
     mavenCentral()
@@ -23,6 +15,7 @@ dependencies {
     testCompile("org.junit.jupiter", "junit-jupiter-api", "5.4.0")
     testRuntimeOnly("org.junit.jupiter", "junit-jupiter-engine", "5.4.0")
 
+    // IMPORTANT: Use this version. Why? See https://github.com/mockito/mockito/issues/1604
     testCompile("org.mockito", "mockito-junit-jupiter", "2.23.4")
 
     testCompile("org.hamcrest","hamcrest-library","1.3")
